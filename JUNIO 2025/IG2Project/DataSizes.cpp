6bca50e6b00ff2e3731e92ea4c8b47d0de59ca43
acostacion <carmengb27@ucm.es>
2026-06-14 11:15:19 +0200

    proyecto inicial configurado para el proximo examen

---
+++ JUNIO 2025/IG2Project/DataSizes.cpp
@@ -0,0 +1,12 @@
+#include "DataSizes.h"
+
+const Vector3 DataSizes::AIRPLANE_BODY_SIZE = {120, 550, 120};
+const Vector3 DataSizes::AIRPLANE_WING_SIZE = {300, 10, 100};
+const Vector3 DataSizes::AIRPLANE_RUDDER_SIZE = {150, 10, 70};
+const Vector3 DataSizes::ENGINE_BASE_SIZE = {70, 100, 70};
+const Vector3 DataSizes::ROCKET_SIZE = {15, 15, 15};
+const int DataSizes::AIRPLANE_NUM_ROCKETS = 5;
+const float DataSizes::AIRPLANE_SPEED = 25;
+const float DataSizes::AIRPLANE_ROTATION = 80;
+
+
---
+++ JUNIO 2025/IG2Project/DataSizes.h
@@ -0,0 +1,21 @@
+#ifndef DataSizes_h
+#define DataSizes_h
+
+#include "IG2Object.h"
+
+class DataSizes{
+    
+public:    
+        
+    static const Vector3 AIRPLANE_BODY_SIZE;
+    static const Vector3 AIRPLANE_WING_SIZE;
+    static const Vector3 AIRPLANE_RUDDER_SIZE;
+    static const Vector3 ENGINE_BASE_SIZE;
+    static const Vector3 ROCKET_SIZE;
+    static const int AIRPLANE_NUM_ROCKETS;
+    static const float AIRPLANE_SPEED;
+    static const float AIRPLANE_ROTATION;   
+        
+    
+};
+#endif /* DataSizes_h */
